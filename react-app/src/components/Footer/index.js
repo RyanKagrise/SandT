d5e20a535b65eefe921351253670449d7769a1a7
Ryan Kagrise <rdkcode@gmail.com>
2022-05-21 21:20:55 -1000

    footer corrected

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -20,9 +20,9 @@ const Footer = () => {
           <a href='https://github.com/RyanKagrise'>
             <img className='github' src='https://upload.wikimedia.org/wikipedia/commons/9/91/Octicons-mark-github.svg' alt='' />
           </a>
-          <div className='footer-text'>
+          <a href='https://ryankagrise.github.io' className='footer-text'>
             Meet The Site's Creator!
-          </div>
+          </a>
           <a href='https://www.linkedin.com/in/ryan-kagrise-27893551/'>
             <img className='github' src='https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/Linkedin_icon.svg/640px-Linkedin_icon.svg.png' alt='' />
           </a>
